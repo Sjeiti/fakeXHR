2bc1fb5e91e3127957233d7262b2735e2e0a35f3
* <github@sjeiti.com>
2019-07-19 15:04:51 +0200

    improved local and static handling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const OldXHR = window.XMLHttpRequest
 class FakeXMLHttpRequest extends OldXHR {
 
   static org = OldXHR
+  static _fakeResponses = new Map()
+
+  static fake(response, method, url, async, user, password){
+    this._fakeResponses.set(this._getKey(method, url, async, user, password), response)
+  }
+
+  static _getKey(...arg){
+    return arg.toString(arg)
+  }
+
+  _fakeResponses = new Map()
   _responseText = null
 
   constructor(...arg){
@@ -10,15 +21,21 @@ class FakeXMLHttpRequest extends OldXHR {
   }
 
   open(method, url, async, user, password){
+    const {constructor} = this
+    const key = constructor._getKey(method, url, async, user, password)
+    this._responseText = 
+      this._fakeResponses.get(key)
+      ||constructor._fakeResponses.get(key)
     super.open(method, url, async, user, password)
   } 
  
   get responseText(){
     return this._responseText||super.responseText
   }
-
-  fake(method, url, responseText){
-    this._responseText = responseText
+ 
+  fake(response, method, url, async, user, password){
+    const {constructor} = this
+    this._fakeResponses.set(constructor._getKey(method, url, async, user, password), response)
   }
 
   send(){
